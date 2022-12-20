b075a39126bf5e4107cf219b8d91e42d6029f473
Paulo <paulocesarhero@gmail.com>
2022-12-19 19:49:18 -0600

    Adicion de jira templates

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, make_response, redirect
+from flask import (Flask, request,
+                   make_response,
+                   redirect,
+                   render_template)
 
 app = Flask(__name__)  # en este caso __name__ = main.py
 
@@ -6,7 +9,7 @@ app = Flask(__name__)  # en este caso __name__ = main.py
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return f'Hello World Platzi, tu IP es {user_ip}'
+    return render_template('hello.html', user_ip = user_ip)
 
 
 @app.route('/')
