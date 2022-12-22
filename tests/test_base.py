1e8ea10132ac09d2aea94daf664e563e0b2218d5
Paulo <paulocesarhero@gmail.com>
2022-12-22 12:36:36 -0600

    auth blueprint completo y probado ademas se corrigio un typo en base.html

--- tests/test_base.py
+++ tests/test_base.py
@@ -30,13 +30,9 @@ class MainTest(TestCase):
         self.assert200(response)
 
     def test_hello_post(self):
-        fake_form = {
-            'username': 'fake',
-            'password': 'fake-password'
-        }
-        response = self.client.post(url_for('hello'), data=fake_form)
+        response = self.client.post(url_for('hello'))
 
-        self.assertRedirects(response, 'http://localhost/' + url_for('index'))
+        self.assertTrue(response.status_code, 405)
 
     def test_auth_blueprint_exists(self):
         self.assertIn('auth', self.app.blueprints)
@@ -51,6 +47,13 @@ class MainTest(TestCase):
 
         self.assertTemplateUsed('login.html')
 
+    def test_auth_login_post(self):
+        fake_form = {
+            'username': 'fake',
+            'password': 'fake-password'
+        }
+        response = self.client.post(url_for('auth.login'), data=fake_form)
+        self.assertRedirects(response, url_for('index'))
     def assertRedirects(self, response, location, message=None):
         """
         Checks if response is an HTTP redirect to the
