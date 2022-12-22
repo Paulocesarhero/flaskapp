7a87b2302f36172fd4a67f7544d5142bf831c9ea
Paulo <paulocesarhero@gmail.com>
2022-12-22 17:03:10 -0600

    Conexion con firebase listo

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from flask import request, make_response, redirect, render_template, session, ur
 import unittest
 
 from app import create_app
+from app.firestore_service import get_users, get_todos
 from app.forms import LoginForm
 
 app = create_app()
 
-
-todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
 @app.cli.command()
@@ -43,8 +42,14 @@ def hello():
 
     context = {
         'user_ip': user_ip,
-        'todos': todos,
+        'todos': get_todos(user_id=username),
         'username': username
     }
+
+    users = get_users()
+
+    for user in users:
+        print(user.id)
+        print(user.to_dict()['password'])
 
     return render_template('hello.html', **context)
