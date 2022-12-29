89a29475bad45451fcd6075821a853190f78c11d
Paulo <paulocesarhero@gmail.com>
2022-12-28 18:08:40 -0600

    Eliminar todos listo

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
-
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 from flask_login import login_required, current_user
 
+
 from app import create_app
-from app.firestore_service import get_users, get_todos, put_todo
-from app.forms import LoginForm, TodoForm
+from app.forms import TodoForm, DeleteTodoForm
+from app.firestore_service import get_users, get_todos, put_todo, delete_todo
 
 app = create_app()
 
@@ -42,18 +42,29 @@ def hello():
     user_ip = session.get('user_ip')
     username = current_user.id
     todo_form = TodoForm()
+    delete_form = DeleteTodoForm()
 
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username),
         'username': username,
-        'todo_form': todo_form
+        'todo_form': todo_form,
+        'delete_form': delete_form,
     }
 
     if todo_form.validate_on_submit():
         put_todo(user_id=username, description=todo_form.description.data)
-        flash('tu tarea se creo con exito')
+
+        flash('Tu tarea se creo con éxito!')
 
         return redirect(url_for('hello'))
 
     return render_template('hello.html', **context)
+
+
+@app.route('/todos/delete/<todo_id>', methods=['POST'])
+def delete(todo_id):
+    user_id = current_user.id
+    delete_todo(user_id=user_id, todo_id=todo_id)
+
+    return redirect(url_for('hello'))
